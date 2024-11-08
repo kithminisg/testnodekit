08d6fefa36cb9ce5e8c21d21cee4d9a77f020184
BimsaraFernando <kavindu@geveo.com>
2024-11-08 16:06:03 +0530

    depencancy require path changed to default

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 // Export the required modules to make them available for import
 module.exports = {
-    'Evernode': require('../evernode-js-client'),
-    'HotPocketClient': require('../hotpocket-js-client')
+    'Evernode': require('evernode-js-client'),
+    'HotPocketClient': require('hotpocket-js-client')
 };
